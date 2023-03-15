f52acb44369cece3d2541933e71ea6bc821342cc
georgeLOTFY <georgelotfy28@gmail.com>
2023-03-16 00:16:13 +0200

    .

--- asca/src/index.tsx
+++ asca/src/index.tsx
@@ -8,6 +8,8 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 defineCustomElements(window);
 
 
+
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
